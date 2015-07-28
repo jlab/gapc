d700a36b3aa02c48464c112d37a64e3ffe24d913
Thomas Gatter <thomas@bioinf.uni-leipzig.de>
2015-07-28 11:26:08 +0200

    forgotten header file

---
+++ operator_fwd.hh
@@ -0,0 +1,14 @@
+/* 
+ * File:   operator_fwd.hh
+ * Author: gatter
+ *
+ * Created on June 23, 2015, 10:53 AM
+ */
+
+#ifndef OPERATOR_FWD_HH
+#define	OPERATOR_FWD_HH
+
+class Operator;
+
+#endif	/* OPERATOR_FWD_HH */
+
