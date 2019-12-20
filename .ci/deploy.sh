a0055bc28a3b493f045965eaf6c992263c26ad91
Stefan Janssen <stefan.m.janssen@gmail.com>
2019-12-20 09:17:34 +0100

    using dput-ng with SFTP

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install gnupg pbuilder ubuntu-dev-tools apt-file dput libdistro-info-perl fakeroot debhelper -y
+sudo apt-get install gnupg pbuilder ubuntu-dev-tools apt-file dput libdistro-info-perl fakeroot debhelper dput-ng -y
 pwd
 ls -la
 ls -la tmp_debian/
 git stash pop
-dput ppa:janssenlab/software tmp_debian/*xenial*_source.changes
+ls -la ~/.dput.cf
+cat ~/.dput.cf
+dput janssenlab-ppa tmp_debian/*xenial*_source.changes
