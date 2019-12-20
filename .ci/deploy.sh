fd18c8e328e1c4482202389185bba9ed6e9f25dd
Stefan Janssen <stefan.m.janssen@gmail.com>
2019-12-20 10:06:07 +0100

    adding paramiko

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install gnupg pbuilder ubuntu-dev-tools apt-file libdistro-info-perl fakeroot debhelper dput-ng -y
+sudo apt-get install gnupg pbuilder ubuntu-dev-tools apt-file libdistro-info-perl fakeroot debhelper dput-ng python-paramiko -y
 pwd
 ls -la
 ls -la tmp_debian/
 git stash pop
 ls -la ~/.dput.cf
 cat ~/.dput.cf
+which -a dput
+head -n 20 `which dput`
 dput janssenlab-ppa tmp_debian/*xenial*_source.changes
