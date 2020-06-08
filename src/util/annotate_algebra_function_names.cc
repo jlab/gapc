382cc2ba82703c16348d47a73a72a59bd95c13e5
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-06-08 22:42:41 +0200

    codestyle

--- src/util/annotate_algebra_function_names.cc
+++ src/util/annotate_algebra_function_names.cc
@@ -23,7 +23,8 @@
 
 #include "annotate_algebra_function_names.hh"
 
-
+#include <string>
+#include <list>
 #include <sstream>
 
 #include "../log.hh"
@@ -39,19 +40,21 @@ Util::AlgebraFunctionNameAnnotator::~AlgebraFunctionNameAnnotator() {
 }
 
 
-void Util::AlgebraFunctionNameAnnotator::annotateGrammar (CFG::CFG* grammar) {
+void Util::AlgebraFunctionNameAnnotator::annotateGrammar(CFG::CFG* grammar) {
   std::list<CFG::GrammarProduction*> productions = grammar->getProductions();
-  for (std::list<CFG::GrammarProduction*>::iterator i = productions.begin(); i != productions.end(); i++) {
-    annotateProduction ((*i)->rhs);
+  for (std::list<CFG::GrammarProduction*>::iterator i = productions.begin();
+       i != productions.end(); i++) {
+    annotateProduction((*i)->rhs);
   }
 }
 
 
-void Util::AlgebraFunctionNameAnnotator::annotateProduction (CFG::Base* b) {
+void Util::AlgebraFunctionNameAnnotator::annotateProduction(CFG::Base* b) {
   switch (b->getType()) {
     case CFG::NONTERMINAL: {
       CFG::NonTerminal* nonTerminal = dynamic_cast<CFG::NonTerminal*> (b);
-      b->setAttribute (new Util::GrammarProductionNamingAttribute (nonTerminal->getName()));
+      b->setAttribute (new Util::GrammarProductionNamingAttribute(
+        nonTerminal->getName()));
       // fall directly through to the other cases for
       // EPSILON, TERMINA, and so on...
     }
@@ -59,30 +62,33 @@ void Util::AlgebraFunctionNameAnnotator::annotateProduction (CFG::Base* b) {
     case CFG::TERMINAL:
     case CFG::REGULAR_EXPRESSION:
     case CFG::PRODUCTION_SEQUENCE: {
-      b->setAttribute (createNextAttribute());
+      b->setAttribute(createNextAttribute());
       break;
     }
     case CFG::BASE_WRAPPER: {
       CFG::BaseWrapper* wrapper = dynamic_cast<CFG::BaseWrapper*> (b);
-      annotateProduction (wrapper->getWrappedBase());
+      annotateProduction(wrapper->getWrappedBase());
       break;
     }
     case CFG::PRODUCTION_ALTERNATIVE: {
-      CFG::ProductionAlternative* alt = dynamic_cast<CFG::ProductionAlternative*> (b);
-      for (CFG::ProductionAlternative::iterator i = alt->begin(); i != alt->end(); i++) {
-        annotateProduction (*i);
+      CFG::ProductionAlternative* alt =
+        dynamic_cast<CFG::ProductionAlternative*> (b);
+      for (CFG::ProductionAlternative::iterator i = alt->begin();
+           i != alt->end(); i++) {
+        annotateProduction(*i);
       }
       break;
     }
     default: {
-      throw LogError ("gap-00501: Unhandled type CFG grammar part.");
+      throw LogError("gap-00501: Unhandled type CFG grammar part.");
       break;
     }
   }
 }
 
 
-Util::AlgebraFunctionNameAttribute* Util::AlgebraFunctionNameAnnotator::createNextAttribute() {
+Util::AlgebraFunctionNameAttribute*
+  Util::AlgebraFunctionNameAnnotator::createNextAttribute() {
   // For creating a unique name for each algebra function,
   // we simply use a global counter, which is increased each
   // time a function name is created.
--- src/util/annotate_algebra_function_names.hh
+++ src/util/annotate_algebra_function_names.hh
@@ -32,41 +32,37 @@
 
 
 namespace Util {
+// Forward declaration of the attribute, becuase we need it
+// in the interface definition of the annotator class.
+class AlgebraFunctionNameAttribute;
 
 
-  // Forward declaration of the attribute, becuase we need it
-  // in the interface definition of the annotator class.
-  class AlgebraFunctionNameAttribute;
+// Simply annotates each alternative of a production with a
+// new algebra function name which is also unique.
+// This algorithm relies on the fact, that each grammar which
+// is given as input to 'annotateGrammar()' consists of
+// grammar productions which are basically a list of alternative
+// context free terms, where those terms do not contain alternatives
+// itself, but only sequences or basic grammar parts (e.g.
+// non-terminals, terminals or epsilons).
+class AlgebraFunctionNameAnnotator {
+
+  public:
+
+    AlgebraFunctionNameAnnotator();
+    ~AlgebraFunctionNameAnnotator();
+
+    void annotateGrammar (CFG::CFG* grammar);
 
 
-  // Simply annotates each alternative of a production with a
-  // new algebra function name which is also unique.
-  // This algorithm relies on the fact, that each grammar which
-  // is given as input to 'annotateGrammar()' consists of
-  // grammar productions which are basically a list of alternative
-  // context free terms, where those terms do not contain alternatives
-  // itself, but only sequences or basic grammar parts (e.g.
-  // non-terminals, terminals or epsilons).
-  class AlgebraFunctionNameAnnotator {
+  private:
 
-    public:
-
-      AlgebraFunctionNameAnnotator();
-      ~AlgebraFunctionNameAnnotator();
-
-      void annotateGrammar (CFG::CFG* grammar);
+    void annotateProduction (CFG::Base* production);
+    AlgebraFunctionNameAttribute* createNextAttribute();
 
 
-    private:
-
-      void annotateProduction (CFG::Base* production);
-      AlgebraFunctionNameAttribute* createNextAttribute();
-
-
-  };
-
-
+};
 }
 
 
-#endif  // ifndef SRC_UTIL_ANNOTATE_ALGEBRA_FUNCTION_NAMES_HH_
+#endif  // SRC_UTIL_ANNOTATE_ALGEBRA_FUNCTION_NAMES_HH_
