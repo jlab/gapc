cd67bcdf8ff31f0105f0ef6f133bfe0e12426ba2
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-06-10 10:43:53 +0200

    codestyle: printer

--- src/printer/cfg_pretty_print_cout.hh
+++ src/printer/cfg_pretty_print_cout.hh
@@ -1,28 +1,45 @@
+/* {{{
+
+    This file is part of gapc (GAPC - Grammars, Algebras, Products - Compiler;
+      a system to compile algebraic dynamic programming programs)
+
+    Copyright (C) 2008-2011  Georg Sauthoff
+         email: gsauthof@techfak.uni-bielefeld.de or gsauthof@sdf.lonestar.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+}}} */
 
 
-
-#ifndef _CFG_PRETTY_PRINT_COUT_HH_
-#define _CFG_PRETTY_PRINT_COUT_HH_
+#ifndef SRC_PRINTER_CFG_PRETTY_PRINT_COUT_HH_
+#define SRC_PRINTER_CFG_PRETTY_PRINT_COUT_HH_
 
 #include "cfg_pretty_print.hh"
 
 
 namespace Printer {
-	
-	
-	// This is a simple extension of the CFG pretty printer which
-	// uses std::cout as output stream.
-	class PrettyPrintCOut : public CFGPrettyPrint {
-		
-		public:
-			
-			PrettyPrintCOut();
-			
-			
-	};
-	
-	
-}
 
 
-#endif	// ifndef _CFG_PRETTY_PRINT_COUT_HH_
+// This is a simple extension of the CFG pretty printer which
+// uses std::cout as output stream.
+class PrettyPrintCOut : public CFGPrettyPrint {
+ public:
+  PrettyPrintCOut();
+};
+
+
+}  // namespace Printer
+
+
+#endif  // SRC_PRINTER_CFG_PRETTY_PRINT_COUT_HH_
