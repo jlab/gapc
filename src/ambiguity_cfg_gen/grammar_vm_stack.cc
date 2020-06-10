fb7dfa4e9238cb70acb860ca72fa1fcf1f32b949
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-06-10 12:54:45 +0200

    codestyle: ambiguity

--- src/ambiguity_cfg_gen/grammar_vm_stack.cc
+++ src/ambiguity_cfg_gen/grammar_vm_stack.cc
@@ -1,4 +1,25 @@
+/* {{{
 
+    This file is part of gapc (GAPC - Grammars, Algebras, Products - Compiler;
+      a system to compile algebraic dynamic programming programs)
+
+    Copyright (C) 2008-2011  Georg Sauthoff
+         email: gsauthof@techfak.uni-bielefeld.de or gsauthof@sdf.lonestar.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+}}} */
 
 #include "grammar_vm_stack.hh"
 
@@ -14,27 +35,27 @@ AmbiguityCFG::GrammarVMStack::~GrammarVMStack() {
 }
 
 
-void AmbiguityCFG::GrammarVMStack::push (GrammarVMStackElement* element) {
-	this->stack.push_back (element);
+void AmbiguityCFG::GrammarVMStack::push(GrammarVMStackElement* element) {
+  this->stack.push_back(element);
 }
 
 
 AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::GrammarVMStack::pop() {
-	assert (this->stack.size() > 0);
-	GrammarVMStackElement* element = this->stack.back();
-	this->stack.pop_back();
-	return element;
+  assert(this->stack.size() > 0);
+  GrammarVMStackElement* element = this->stack.back();
+  this->stack.pop_back();
+  return element;
 }
 
 
 AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::GrammarVMStack::peek() {
-	assert (this->stack.size() > 0);
-	return this->stack.back();
+  assert(this->stack.size() > 0);
+  return this->stack.back();
 }
 
 
 void AmbiguityCFG::GrammarVMStack::reset() {
-	this->stack.clear();
+  this->stack.clear();
 }
 
 
@@ -42,8 +63,9 @@ void AmbiguityCFG::GrammarVMStack::reset() {
 //////////////////////////////////////////////////////////
 
 
-AmbiguityCFG::GrammarVMStackElement::GrammarVMStackElement (AmbiguityCFG::GrammarVMStackElementType t)
-	: type (t) {
+AmbiguityCFG::GrammarVMStackElement::GrammarVMStackElement(
+  AmbiguityCFG::GrammarVMStackElementType t)
+  : type(t) {
 }
 
 
@@ -51,37 +73,15 @@ AmbiguityCFG::GrammarVMStackElement::~GrammarVMStackElement() {
 }
 
 
-bool AmbiguityCFG::GrammarVMStackElement::is (AmbiguityCFG::GrammarVMStackElementType t) {
-	return this->type == t;
+bool AmbiguityCFG::GrammarVMStackElement::is(
+  AmbiguityCFG::GrammarVMStackElementType t) {
+  return this->type == t;
 }
 
 
-AmbiguityCFG::GrammarVMStackElementType AmbiguityCFG::GrammarVMStackElement::getType() {
-	return this->type;
-}
-
-
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-
-
-AmbiguityCFG::ProductionFragmentGrammarVMStackElement::ProductionFragmentGrammarVMStackElement (CFG::Base* b)
-	: GrammarVMStackElement (PRODUCTION_FRAGMENT_ELEMENT), productionFragment (b) {
-}
-
-
-AmbiguityCFG::ProductionFragmentGrammarVMStackElement::~ProductionFragmentGrammarVMStackElement() {
-}
-
-
-CFG::Base* AmbiguityCFG::ProductionFragmentGrammarVMStackElement::getProductionFragment() {
-	return this->productionFragment;
-}
-
-
-AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::ProductionFragmentGrammarVMStackElement::clone() {
-	ProductionFragmentGrammarVMStackElement* newInstance = new ProductionFragmentGrammarVMStackElement (this->productionFragment);
-	return newInstance;
+AmbiguityCFG::GrammarVMStackElementType AmbiguityCFG::GrammarVMStackElement::
+  getType() {
+  return this->type;
 }
 
 
@@ -89,47 +89,28 @@ AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::ProductionFragmentGrammarVMSt
 //////////////////////////////////////////////////////////
 
 
-AmbiguityCFG::NamedAccessGrammarVMStackElement::NamedAccessGrammarVMStackElement (AmbiguityCFG::NamedAccess* element)
-	: GrammarVMStackElement (NAMED_ACCESS_ELEMENT), access (element) {
+AmbiguityCFG::ProductionFragmentGrammarVMStackElement::
+  ProductionFragmentGrammarVMStackElement(CFG::Base* b)
+  : GrammarVMStackElement(PRODUCTION_FRAGMENT_ELEMENT), productionFragment(b) {
 }
 
 
-AmbiguityCFG::NamedAccessGrammarVMStackElement::~NamedAccessGrammarVMStackElement() {
+AmbiguityCFG::ProductionFragmentGrammarVMStackElement::
+  ~ProductionFragmentGrammarVMStackElement() {
 }
 
 
-AmbiguityCFG::NamedAccess* AmbiguityCFG::NamedAccessGrammarVMStackElement::getNamedAccessElement() {
-	return this->access;
+CFG::Base* AmbiguityCFG::ProductionFragmentGrammarVMStackElement::
+  getProductionFragment() {
+  return this->productionFragment;
 }
 
 
-AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::NamedAccessGrammarVMStackElement::clone() {
-	NamedAccessGrammarVMStackElement* newInstance = new NamedAccessGrammarVMStackElement (this->access);
-	return newInstance;
-}
-
-
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-
-
-AmbiguityCFG::VarDeclItemGrammarVMStackElement::VarDeclItemGrammarVMStackElement (VarDeclInfo* infoItem)
-	: GrammarVMStackElement (VAR_DECL_INFO_ELEMENT), infoItem (infoItem) {
-}
-
-
-AmbiguityCFG::VarDeclItemGrammarVMStackElement::~VarDeclItemGrammarVMStackElement() {
-}
-
-
-AmbiguityCFG::VarDeclInfo* AmbiguityCFG::VarDeclItemGrammarVMStackElement::getVarInfoItem() {
-	return this->infoItem;
-}
-
-
-AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::VarDeclItemGrammarVMStackElement::clone() {
-	VarDeclItemGrammarVMStackElement* newInstance = new VarDeclItemGrammarVMStackElement (this->infoItem);
-	return newInstance;
+AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::
+  ProductionFragmentGrammarVMStackElement::clone() {
+  ProductionFragmentGrammarVMStackElement* newInstance =
+    new ProductionFragmentGrammarVMStackElement(this->productionFragment);
+  return newInstance;
 }
 
 
@@ -137,23 +118,28 @@ AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::VarDeclItemGrammarVMStackElem
 //////////////////////////////////////////////////////////
 
 
-AmbiguityCFG::MultiProductionFragmentGrammarVMStackElement::MultiProductionFragmentGrammarVMStackElement (std::vector<CFG::Base*> fragments)
-	: GrammarVMStackElement (MULTI_PRODUCTION_FRAGMENT_ELEMENT), fragments (fragments) {
+AmbiguityCFG::NamedAccessGrammarVMStackElement::
+  NamedAccessGrammarVMStackElement(AmbiguityCFG::NamedAccess* element)
+  : GrammarVMStackElement(NAMED_ACCESS_ELEMENT), access(element) {
 }
 
 
-AmbiguityCFG::MultiProductionFragmentGrammarVMStackElement::~MultiProductionFragmentGrammarVMStackElement() {
+AmbiguityCFG::NamedAccessGrammarVMStackElement::
+  ~NamedAccessGrammarVMStackElement() {
 }
 
 
-std::vector<CFG::Base*> AmbiguityCFG::MultiProductionFragmentGrammarVMStackElement::getProductionFragments() {
-	return this->fragments;
+AmbiguityCFG::NamedAccess* AmbiguityCFG::NamedAccessGrammarVMStackElement::
+  getNamedAccessElement() {
+  return this->access;
 }
 
 
-AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::MultiProductionFragmentGrammarVMStackElement::clone() {
-	MultiProductionFragmentGrammarVMStackElement* newInstance = new MultiProductionFragmentGrammarVMStackElement (this->fragments);
-	return newInstance;
+AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::
+  NamedAccessGrammarVMStackElement::clone() {
+  NamedAccessGrammarVMStackElement* newInstance =
+    new NamedAccessGrammarVMStackElement(this->access);
+  return newInstance;
 }
 
 
@@ -161,8 +147,69 @@ AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::MultiProductionFragmentGramma
 //////////////////////////////////////////////////////////
 
 
-AmbiguityCFG::ContextGrammarVMStackElement::ContextGrammarVMStackElement (MultiVariableContext* cntxt)
-	: GrammarVMStackElement (CONTEXT_ELEMENT), context (cntxt) {
+AmbiguityCFG::VarDeclItemGrammarVMStackElement::
+  VarDeclItemGrammarVMStackElement(VarDeclInfo* infoItem)
+  : GrammarVMStackElement(VAR_DECL_INFO_ELEMENT), infoItem(infoItem) {
+}
+
+
+AmbiguityCFG::VarDeclItemGrammarVMStackElement::
+  ~VarDeclItemGrammarVMStackElement() {
+}
+
+
+AmbiguityCFG::VarDeclInfo* AmbiguityCFG::VarDeclItemGrammarVMStackElement::
+  getVarInfoItem() {
+  return this->infoItem;
+}
+
+
+AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::
+  VarDeclItemGrammarVMStackElement::clone() {
+  VarDeclItemGrammarVMStackElement* newInstance =
+    new VarDeclItemGrammarVMStackElement(this->infoItem);
+  return newInstance;
+}
+
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+
+AmbiguityCFG::MultiProductionFragmentGrammarVMStackElement::
+  MultiProductionFragmentGrammarVMStackElement(
+    std::vector<CFG::Base*> fragments)
+  : GrammarVMStackElement(MULTI_PRODUCTION_FRAGMENT_ELEMENT), fragments(
+    fragments) {
+}
+
+
+AmbiguityCFG::MultiProductionFragmentGrammarVMStackElement::
+  ~MultiProductionFragmentGrammarVMStackElement() {
+}
+
+
+std::vector<CFG::Base*> AmbiguityCFG::
+  MultiProductionFragmentGrammarVMStackElement::getProductionFragments() {
+  return this->fragments;
+}
+
+
+AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::
+  MultiProductionFragmentGrammarVMStackElement::clone() {
+  MultiProductionFragmentGrammarVMStackElement* newInstance =
+    new MultiProductionFragmentGrammarVMStackElement(this->fragments);
+  return newInstance;
+}
+
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+
+AmbiguityCFG::ContextGrammarVMStackElement::ContextGrammarVMStackElement(
+  MultiVariableContext* cntxt)
+  : GrammarVMStackElement(CONTEXT_ELEMENT), context(cntxt) {
 }
 
 
@@ -170,13 +217,15 @@ AmbiguityCFG::ContextGrammarVMStackElement::~ContextGrammarVMStackElement() {
 }
 
 
-AmbiguityCFG::MultiVariableContext* AmbiguityCFG::ContextGrammarVMStackElement::getContext() {
-	return this->context;
+AmbiguityCFG::MultiVariableContext* AmbiguityCFG::
+  ContextGrammarVMStackElement::getContext() {
+  return this->context;
 }
 
 
-AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::ContextGrammarVMStackElement::clone() {
-	ContextGrammarVMStackElement* newInstance = new ContextGrammarVMStackElement (this->context);
-	return newInstance;
+AmbiguityCFG::GrammarVMStackElement* AmbiguityCFG::
+  ContextGrammarVMStackElement::clone() {
+  ContextGrammarVMStackElement* newInstance = new ContextGrammarVMStackElement(
+    this->context);
+  return newInstance;
 }
-
--- src/ambiguity_cfg_gen/grammar_vm_stack.hh
+++ src/ambiguity_cfg_gen/grammar_vm_stack.hh
@@ -1,7 +1,28 @@
+/* {{{
 
+    This file is part of gapc (GAPC - Grammars, Algebras, Products - Compiler;
+      a system to compile algebraic dynamic programming programs)
 
-#ifndef _GRAMMAR_VM_STACK_HH_
-#define _GRAMMAR_VM_STACK_HH_
+    Copyright (C) 2008-2011  Georg Sauthoff
+         email: gsauthof@techfak.uni-bielefeld.de or gsauthof@sdf.lonestar.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+}}} */
+
+#ifndef SRC_AMBIGUITY_CFG_GEN_GRAMMAR_VM_STACK_HH_
+#define SRC_AMBIGUITY_CFG_GEN_GRAMMAR_VM_STACK_HH_
 
 #include <vector>
 
@@ -10,203 +31,161 @@
 #include "variable_context.hh"
 
 
-namespace AmbiguityCFG
-{
-	
-	
-	// Forward declaration of the element class used by the stack.
-	class GrammarVMStackElement;
-	
-	
-	// The operand stack of the GrammarVM is a simple stack
-	// implementation which provides some basic functionality
-	// for pushing and popping elements to and from the stack.
-	class GrammarVMStack {
-		
-		private:
-			
-			// The internal stack that holds all elements.
-			std::vector<GrammarVMStackElement*> stack;
-			
-			
-		public:
-			
-			GrammarVMStack();
-			~GrammarVMStack();
-			
-			// Pushes a new element on top of the stack.
-			void push (GrammarVMStackElement* element);
-			// Removes the top element from the stack and returns it
-			// as result.
-			GrammarVMStackElement* pop();
-			// Returns the top element of the stack, leaving the stack
-			// content unchanged.
-			GrammarVMStackElement* peek();
-			
-			// Resets the contents of the stack, by removing all elements
-			// from the stack
-			void reset();
-			
-			
-	};
-	
-	
-	// This enumeration defines the possible subclass types a
-	// GrammarVMStackElement may have.
-	enum GrammarVMStackElementType {PRODUCTION_FRAGMENT_ELEMENT, NAMED_ACCESS_ELEMENT, VAR_DECL_INFO_ELEMENT, MULTI_PRODUCTION_FRAGMENT_ELEMENT, CONTEXT_ELEMENT};
-	
-	
-	// The grammar VM stack element is a wrapper class that
-	// facilitates type administration of stack elements. With
-	// the help of this class the grammar VM knows exactly which
-	// kind of element is on top of the stack.
-	class GrammarVMStackElement {
-		
-		protected:
-			
-			// The specific type of the subclass.
-			GrammarVMStackElementType type;
-			
-			// Inits this instance and sets the instance type to
-			// the value of the parameter.
-			GrammarVMStackElement (GrammarVMStackElementType t);
-			
-			
-		public:
-			
-			virtual ~GrammarVMStackElement();
-			
-			bool is (GrammarVMStackElementType type);
-			GrammarVMStackElementType getType();
-			
-			virtual GrammarVMStackElement* clone() = 0;
-			
-			
-	};
-	
-	
-	// The wrapper for production fragment elements on the stack.
-	class ProductionFragmentGrammarVMStackElement : public GrammarVMStackElement {
-		
-		private:
-			
-			// Stores the terminal this instance wraps.
-			CFG::Base* productionFragment;
-			
-			
-		public:
-			
-			ProductionFragmentGrammarVMStackElement (CFG::Base* b);
-			~ProductionFragmentGrammarVMStackElement();
-			
-			// Returns the terminal element that is wrapped by this
-			// instance.
-			CFG::Base* getProductionFragment();
-			
-			GrammarVMStackElement* clone();
-			
-			
-	};
-	
-	
-	// Wraps a NamedAccess instance into a GrammarVMStackElement.
-	class NamedAccessGrammarVMStackElement : public GrammarVMStackElement {
-		
-		private:
-			
-			// Stores the pointer to the named-access-instance.
-			NamedAccess* access;
-			
-			
-		public:
-			
-			NamedAccessGrammarVMStackElement (NamedAccess* element);
-			~NamedAccessGrammarVMStackElement();
-			
-			// Returns the named-access element this instance wraps.
-			NamedAccess* getNamedAccessElement();
-			
-			GrammarVMStackElement* clone();
-			
-			
-	};
-	
-	
-	// Wraps a VarDeclInfoItem for storage on the stack.
-	class VarDeclItemGrammarVMStackElement : public GrammarVMStackElement {
-		
-		private:
-			
-			// Stores the info item wrapped by this instance.
-			VarDeclInfo* infoItem;
-			
-			
-		public:
-			
-			VarDeclItemGrammarVMStackElement (VarDeclInfo* infoItem);
-			~VarDeclItemGrammarVMStackElement();
-			
-			// Returns the variable information items wrapped in this
-			// instance.
-			VarDeclInfo* getVarInfoItem();
-			
-			GrammarVMStackElement* clone();
-			
-			
-	};
-	
-	
-	// A stack element that holds a list of simultaneous
-	// valid values.
-	class MultiProductionFragmentGrammarVMStackElement : public GrammarVMStackElement {
-		
-		private:
-			
-			// The list of production fragments.
-			std::vector<CFG::Base*> fragments;
-			
-			
-		public:
-			
-			MultiProductionFragmentGrammarVMStackElement (std::vector<CFG::Base*> fragments);
-			~MultiProductionFragmentGrammarVMStackElement();
-			
-			// Returns the list of production fragments this stack
-			// element instance holds.
-			std::vector<CFG::Base*> getProductionFragments();
-			
-			// Creates a copy of this stack element.
-			GrammarVMStackElement* clone();
-			
-			
-	};
-	
-	
-	class ContextGrammarVMStackElement : public GrammarVMStackElement {
-		
-		private:
-			
-			// Stores the context pointer this stack element holds.
-			MultiVariableContext* context;
-			
-			
-		public:
-			
-			ContextGrammarVMStackElement (MultiVariableContext* cntxt);
-			~ContextGrammarVMStackElement();
-			
-			// Returns the context this stack element holds.
-			MultiVariableContext* getContext();
-			
-			// Creates a copy of this stack element.
-			GrammarVMStackElement* clone();
-			
-			
-	};
-	
-	
-}
+namespace AmbiguityCFG {
+// Forward declaration of the element class used by the stack.
+class GrammarVMStackElement;
 
 
-#endif	// ifndef _GRAMMAR_VM_STACK_HH_
+// The operand stack of the GrammarVM is a simple stack
+// implementation which provides some basic functionality
+// for pushing and popping elements to and from the stack.
+class GrammarVMStack {
+ private:
+    // The internal stack that holds all elements.
+    std::vector<GrammarVMStackElement*> stack;
 
+ public:
+    GrammarVMStack();
+    ~GrammarVMStack();
+
+    // Pushes a new element on top of the stack.
+    void push(GrammarVMStackElement* element);
+    // Removes the top element from the stack and returns it
+    // as result.
+    GrammarVMStackElement* pop();
+    // Returns the top element of the stack, leaving the stack
+    // content unchanged.
+    GrammarVMStackElement* peek();
+
+    // Resets the contents of the stack, by removing all elements
+    // from the stack
+    void reset();
+};
+
+
+// This enumeration defines the possible subclass types a
+// GrammarVMStackElement may have.
+enum GrammarVMStackElementType {PRODUCTION_FRAGMENT_ELEMENT,
+  NAMED_ACCESS_ELEMENT, VAR_DECL_INFO_ELEMENT,
+  MULTI_PRODUCTION_FRAGMENT_ELEMENT, CONTEXT_ELEMENT};
+
+
+// The grammar VM stack element is a wrapper class that
+// facilitates type administration of stack elements. With
+// the help of this class the grammar VM knows exactly which
+// kind of element is on top of the stack.
+class GrammarVMStackElement {
+ protected:
+    // The specific type of the subclass.
+    GrammarVMStackElementType type;
+
+    // Inits this instance and sets the instance type to
+    // the value of the parameter.
+    GrammarVMStackElement(GrammarVMStackElementType t);
+
+ public:
+    virtual ~GrammarVMStackElement();
+
+    bool is(GrammarVMStackElementType type);
+    GrammarVMStackElementType getType();
+
+    virtual GrammarVMStackElement* clone() = 0;
+};
+
+
+// The wrapper for production fragment elements on the stack.
+class ProductionFragmentGrammarVMStackElement : public GrammarVMStackElement {
+ private:
+    // Stores the terminal this instance wraps.
+    CFG::Base* productionFragment;
+
+ public:
+    ProductionFragmentGrammarVMStackElement(CFG::Base* b);
+    ~ProductionFragmentGrammarVMStackElement();
+
+    // Returns the terminal element that is wrapped by this
+    // instance.
+    CFG::Base* getProductionFragment();
+
+    GrammarVMStackElement* clone();
+};
+
+
+// Wraps a NamedAccess instance into a GrammarVMStackElement.
+class NamedAccessGrammarVMStackElement : public GrammarVMStackElement {
+ private:
+    // Stores the pointer to the named-access-instance.
+    NamedAccess* access;
+
+ public:
+    NamedAccessGrammarVMStackElement(NamedAccess* element);
+    ~NamedAccessGrammarVMStackElement();
+
+    // Returns the named-access element this instance wraps.
+    NamedAccess* getNamedAccessElement();
+
+    GrammarVMStackElement* clone();
+};
+
+
+// Wraps a VarDeclInfoItem for storage on the stack.
+class VarDeclItemGrammarVMStackElement : public GrammarVMStackElement {
+ private:
+    // Stores the info item wrapped by this instance.
+    VarDeclInfo* infoItem;
+
+ public:
+    VarDeclItemGrammarVMStackElement(VarDeclInfo* infoItem);
+    ~VarDeclItemGrammarVMStackElement();
+
+    // Returns the variable information items wrapped in this
+    // instance.
+    VarDeclInfo* getVarInfoItem();
+
+    GrammarVMStackElement* clone();
+};
+
+
+// A stack element that holds a list of simultaneous
+// valid values.
+class MultiProductionFragmentGrammarVMStackElement :
+  public GrammarVMStackElement {
+ private:
+    // The list of production fragments.
+    std::vector<CFG::Base*> fragments;
+
+ public:
+    MultiProductionFragmentGrammarVMStackElement(
+      std::vector<CFG::Base*> fragments);
+    ~MultiProductionFragmentGrammarVMStackElement();
+
+    // Returns the list of production fragments this stack
+    // element instance holds.
+    std::vector<CFG::Base*> getProductionFragments();
+
+    // Creates a copy of this stack element.
+    GrammarVMStackElement* clone();
+};
+
+
+class ContextGrammarVMStackElement : public GrammarVMStackElement {
+ private:
+    // Stores the context pointer this stack element holds.
+    MultiVariableContext* context;
+
+ public:
+    ContextGrammarVMStackElement(MultiVariableContext* cntxt);
+    ~ContextGrammarVMStackElement();
+
+    // Returns the context this stack element holds.
+    MultiVariableContext* getContext();
+
+    // Creates a copy of this stack element.
+    GrammarVMStackElement* clone();
+};
+
+
+}  // namespace AmbiguityCFG
+
+
+#endif  // SRC_AMBIGUITY_CFG_GEN_GRAMMAR_VM_STACK_HH_
