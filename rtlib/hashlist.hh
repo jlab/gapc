7c4a9fd275fe2ac21745a48dbbf89cb41a8d2192
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-12-03 12:24:11 +0100

    only code style

--- rtlib/hashlist.hh
+++ rtlib/hashlist.hh
@@ -35,56 +35,64 @@
 template<class T, class I>
 class Hash_List_Back {
  private:
-    typedef Hash::Set<T, I, uint32_t,
+  typedef Hash::Set<T, I, uint32_t,
             Hash::Multhash, Hash::Size2pow, Hash::RecordPositions> Hash_t;
 
-    I inspector;
-    // FIXME directly use *list && destructor;
-    List_Ref<T> list;
-    unsigned int size;
+  I inspector;
+  // FIXME directly use *list && destructor;
+  List_Ref<T> list;
+  unsigned int size;
 
-    Hash_List_Back(const Hash_List_Back&);
-    Hash_List_Back &operator=(const Hash_List_Back &);
+  Hash_List_Back(const Hash_List_Back&);
+  Hash_List_Back &operator=(const Hash_List_Back &);
 
  public:
-    uint32_t ref_count;
+  uint32_t ref_count;
 
-    Hash_List_Back()
-      : size(0), ref_count(1) {
-    }
-    void filter() {
-      if (inspector.filter()) {
-        std::abort();
-      } else {
-        Hash_t &set = Singleton<Hash_t>::ref();
-        set.hint(4096);
-        for (typename List<T>::iterator i = list->begin();
-             i != list->end(); ++i)
-          set.add(*i);
-        List_Ref<T> l;
-        size = 0;
-        for (typename Hash_t::discard_iterator i = set.discard_begin();
-            i != set.discard_end(); ++i) {
-          move(l.ref().push_back_ref(), *i);
-          ++size;
-        }
-        set.reset();
-        list = l;
+  Hash_List_Back() : size(0), ref_count(1) {
+  }
+
+  void filter() {
+    if (inspector.filter()) {
+      std::abort();
+    } else {
+      Hash_t &set = Singleton<Hash_t>::ref();
+      set.hint(4096);
+      for (typename List<T>::iterator i = list->begin();
+           i != list->end(); ++i) {
+        set.add(*i);
       }
+      List_Ref<T> l;
+      size = 0;
+      for (typename Hash_t::discard_iterator i = set.discard_begin();
+           i != set.discard_end(); ++i) {
+        move(l.ref().push_back_ref(), *i);
+        ++size;
+      }
+      set.reset();
+      list = l;
     }
-    void add(const T &t) {
-      list->push_back(t);
-      ++size;
-    }
+  }
 
-    // bool isEmpty() const { return !list.l || list.const_ref().isEmpty(); }
-    bool isEmpty() const { return ::isEmpty(list); }
+  void add(const T &t) {
+    list->push_back(t);
+    ++size;
+  }
 
-    typedef typename List<T>::iterator iterator;
+  // bool isEmpty() const { return !list.l || list.const_ref().isEmpty(); }
+  bool isEmpty() const {
+    return ::isEmpty(list);
+  }
 
-    iterator begin() const { return list.const_ref().begin(); }
-    iterator end() const { return list.const_ref().end(); }
-};
+  typedef typename List<T>::iterator iterator;
+
+  iterator begin() const {
+    return list.const_ref().begin();
+  }
+  iterator end() const {
+    return list.const_ref().end();
+  }
+}
 
 template<class T, class I>
 class Hash_List : public ::Ref::Lazy<Hash_List_Back<T, I> > {
@@ -111,38 +119,38 @@ inline void append(Hash_List<T, I> &x, Hash_List<T, I> &e) {
   if (isEmpty(e))
     return;
   assert(&x.ref() != &e.ref());
-  for (typename Hash_List<T, I>::iterator i = e->begin(); i != e->end(); ++i)
+  for (typename Hash_List<T, I>::iterator i = e->begin(); i != e->end(); ++i) {
     x->add(*i);
+  }
 }
 
 template<class T, class I, typename Iterator>
 inline void append_filter(Hash_List<T, I> &x, std::pair<Iterator, Iterator> i) {
-  for (Iterator a = i.first; a != i.second; ++a)
+  for (Iterator a = i.first; a != i.second; ++a) {
     push_back(x, *a);
+  }
   hash_filter(x);
 }
 
-template<class T, class I>
-inline void empty(Hash_List<T, I> &x) {
+template<class T, class I> inline void empty(Hash_List<T, I> &x) {
 }
 
-template<class T, class I>
-inline bool isEmpty(const Hash_List<T, I> &x) {
+template<class T, class I> inline bool isEmpty(const Hash_List<T, I> &x) {
   return !x.l || x.const_ref().isEmpty();
 }
 
-template<class T, class I>
-inline void erase(Hash_List<T, I> &x) {
+template<class T, class I> inline void erase(Hash_List<T, I> &x) {
 }
 
-template<class T, class I>
-inline
+template<class T, class I> inline
 std::ostream &operator<<(std::ostream &out, Hash_List<T, I> &x) {
-  if (isEmpty(x))
+  if (isEmpty(x)) {
     return out;
+  }
   typename Hash_List<T, I>::Type &h = x.ref();
-  for (typename Hash_List<T, I>::iterator i = h.begin(); i != h.end(); ++i)
+  for (typename Hash_List<T, I>::iterator i = h.begin(); i != h.end(); ++i) {
     out << *i << '\n';
+  }
   return out;
 }
 
--- rtlib/hashtng.hh
+++ rtlib/hashtng.hh
@@ -33,22 +33,14 @@
 #include <utility>
 #include <boost/cstdint.hpp>
 
-
 #include "bitops.hh"
 #include "move.hh"
-
 #include "ref.hh"
-
 #include "pool.hh"
-
 #include "vector_sparse.hh"
-
 #include "hash_stats.hh"
 
-
 using std::swap;
-
-
 
 #ifndef HASH_INITIAL
 #define HASH_INITIAL 16
@@ -67,14 +59,15 @@ using std::swap;
 #endif
 
 
-inline uint32_t hashable_value(int t) { return t; }
+inline uint32_t hashable_value(int t) {
+  return t;
+}
 
 namespace Hash {
-
-
 // FIXME
-inline uint32_t hashable_value(size_t t) { return t; }
-
+inline uint32_t hashable_value(size_t t) {
+  return t;
+}
 
 struct Multhash {
   uint32_t operator()(uint32_t kk, uint32_t m) const {
@@ -87,8 +80,7 @@ struct Multhash {
   }
 };
 
-template <typename T>
-struct Size2pow {
+template <typename T> struct Size2pow {
   T initial() const {
     // return 512;
     // return 4;
@@ -96,7 +88,8 @@ struct Size2pow {
     // return 32;
     // return 16;
     return HASH_INITIAL;
-  }
+  };
+
   T expand(T size) const {
     return size * 2;
   }
@@ -109,38 +102,59 @@ struct Size2pow {
 struct EnableShrink {
   enum { value = true };
 };
+
 struct DisableShrink {
   enum { value = false };
 };
 
-template <typename T, typename U = uint32_t>
-struct Default_Inspector {
-  T init(const T &x) const { return x; }
+template <typename T, typename U = uint32_t> struct Default_Inspector {
+  T init(const T &x) const {
+    return x;
+  }
+
   U hash(const T &x) const {
     return hashable_value(x);
   }
+
   void update(T &dst, const T &src) const {
   }
+
   bool equal(const T &a, const T &b) const {
     return a == b;
   }
-  bool filter() const { return false; }
-  bool filter(const T &x) const { assert(0); return false; }
+
+  bool filter() const {
+    return false;
+  }
+
+  bool filter(const T &x) const {
+    assert(0);
+    return false;
+  }
+
   void finalize(T &src) const {
   }
 
-uint32_t k() const { assert(0); return 0; }
-bool cutoff() const { return false; }
-bool equal_score(const T &a, const T &b) const {
-assert(0);;
-return false;
-}
-struct compare {
-bool operator()(const T &a, const T &b) const {
-  assert(0);;
-  return false;
-}
-};
+  uint32_t k() const {
+    assert(0);
+    return 0;
+  }
+
+  bool cutoff() const {
+    return false;
+  }
+
+  bool equal_score(const T &a, const T &b) const {
+    assert(0);
+    return false;
+  }
+
+  struct compare {
+    bool operator()(const T &a, const T &b) const {
+      assert(0);
+      return false;
+    }
+  };
 };
 
 template <typename T,
@@ -151,188 +165,201 @@ template <typename T,
           typename Shrink_Policy = HASH_SHRINK,
           typename Stat_Policy = HASH_STAT,
           unsigned int load_factor = HASH_LOAD_FACTOR
-          >
-class Set {
+          > class Set {
  private:
-    Vector_Sparse<T, U> array;
-    std::vector<bool> init;
-    U used_;
+  Vector_Sparse<T, U> array;
+  std::vector<bool> init;
+  U used_;
 #ifndef NDEBUG
-    bool finalized;
+  bool finalized;
 #endif
 
-    Inspector inspector;
-    Resize_Policy<U> resize_policy;
-    Hash_Policy hash_policy;
+  Inspector inspector;
+  Resize_Policy<U> resize_policy;
+  Hash_Policy hash_policy;
 
-    void rehash(U i) {
-      assert(i > array.size());
-      U size = resize_policy.filter(i);
-      Vector_Sparse<T, U> a(size);
-      swap(a, array);
-      std::vector<bool> b(size);
-      swap(init, b);
-      used_ = 0;
-      for (typename Vector_Sparse<T, U>::iterator i = a.begin();
-           i != a.end(); ++i)
-        add(*i, false);
+  void rehash(U i) {
+    assert(i > array.size());
+    U size = resize_policy.filter(i);
+    Vector_Sparse<T, U> a(size);
+    swap(a, array);
+    std::vector<bool> b(size);
+    swap(init, b);
+    used_ = 0;
+    for (typename Vector_Sparse<T, U>::iterator i = a.begin();
+         i != a.end(); ++i) {
+      add(*i, false);
     }
-    bool loaded() const {
-      unsigned int load = double(used_)/double(array.size()) * 100.0;
-      return load >= load_factor;
-    }
-    void expand() {
-      U i = resize_policy.expand(array.size()+1);
-      rehash(i);
-    }
-    U hash(const U &index) const {
-      return hash_policy(index, array.size());
-    }
-    bool insert(U index, const T &t, bool update) {
+  }
+
+  bool loaded() const {
+    unsigned int load = static_cast<double>(used_) /
+                        static_cast<double>(array.size()) * 100.0;
+    return load >= load_factor;
+  }
+  void expand() {
+    U i = resize_policy.expand(array.size()+1);
+    rehash(i);
+  }
+  U hash(const U &index) const {
+    return hash_policy(index, array.size());
+  }
+  bool insert(U index, const T &t, bool update) {
 #ifndef NDEBUG
-      U check = 0;
+    U check = 0;
 #endif
-      for (U i = index; ; i = (i+1)%array.size()) {
-        assert(check++ < array.size());
-        if (init[i]) {
-          if (inspector.equal(array(i), t)) {
-            assert(update);
-            inspector.update(array(i), t);
-            return false;
-          }
-        } else {
-          init[i] = true;
-          if (update)
-            array.init(i, inspector.init(t));
-          else
-            array.init(i, t);
-          return true;
+    for (U i = index; ; i = (i+1)%array.size()) {
+      assert(check++ < array.size());
+      if (init[i]) {
+        if (inspector.equal(array(i), t)) {
+          assert(update);
+          inspector.update(array(i), t);
+          return false;
         }
+      } else {
+        init[i] = true;
+        if (update)
+          array.init(i, inspector.init(t));
+        else
+          array.init(i, t);
+        return true;
       }
-      assert(0);
     }
+    assert(0);
+  }
 
-    void add(const T &t, bool update) {
-      assert(!finalized);
-      if (!array.size() || loaded())
-        expand();
-      U index = hash(inspector.hash(t));
-      bool r = insert(index, t, update);
-      if (r)
-        ++used_;
-    }
+  void add(const T &t, bool update) {
+    assert(!finalized);
+    if (!array.size() || loaded())
+      expand();
+    U index = hash(inspector.hash(t));
+    bool r = insert(index, t, update);
+    if (r)
+      ++used_;
+  }
 
-
-    Set(const Set&);
-    Set &operator=(const Set&);
+  Set(const Set&);
+  Set &operator=(const Set&);
 
  public:
-    U ref_count;
-    Set()
-      : used_(0),
+  U ref_count;
+  Set() : used_(0),
 #ifndef NDEBUG
-        finalized(false),
+  finalized(false),
 #endif
-        ref_count(1) {
-    }
-    void resize(U i) {
-      if (i < array.size())
-        return;
-      rehash(i);
-    }
-    void add(const T &t) {
-      add(t, true);
-    }
-    bool isEmpty() const { return !used_; }
+  ref_count(1) {
+  }
 
-    typedef typename Vector_Sparse<T, U>::iterator iterator;
+  void resize(U i) {
+    if (i < array.size())
+      return;
+    rehash(i);
+  }
 
-    iterator begin() { assert(finalized); return array.begin(); }
-    iterator end() { return array.end(); }
+  void add(const T &t) {
+    add(t, true);
+  }
 
-    void filter() {
-      if (isEmpty())
-        return;
+  bool isEmpty() const {
+    return !used_;
+  }
 
-      if (inspector.filter()) {
-        Vector_Sparse<T, U> a(used_);
-        swap(array, a);
-        std::vector<bool> b(used_);
-        swap(init, b);
-        used_ = 0;
-        for (typename Vector_Sparse<T, U>::iterator i = a.begin();
-             i != a.end(); ++i)
-          if (!inspector.filter(*i)) {
-            array.init(used_, *i);
-            init[used_] = true;
-            ++used_;
-          }
+  typedef typename Vector_Sparse<T, U>::iterator iterator;
+
+  iterator begin() {
+    assert(finalized);
+    return array.begin();
+  }
+  iterator end() {
+    return array.end();
+  }
+
+  void filter() {
+    if (isEmpty())
+      return;
+
+    if (inspector.filter()) {
+      Vector_Sparse<T, U> a(used_);
+      swap(array, a);
+      std::vector<bool> b(used_);
+      swap(init, b);
+      used_ = 0;
+      for (typename Vector_Sparse<T, U>::iterator i = a.begin(); i != a.end();
+           ++i) {
+        if (!inspector.filter(*i)) {
+          array.init(used_, *i);
+          init[used_] = true;
+          ++used_;
+        }
       }
+    }
 
-      if (!inspector.cutoff() && Shrink_Policy::value) {
+    if (!inspector.cutoff() && Shrink_Policy::value) {
+      Vector_Sparse<T, U> a(used_);
+      swap(array, a);
+      std::vector<bool> b(used_);
+      swap(init, b);
+      U j = 0;
+      for (typename Vector_Sparse<T, U>::iterator i = a.begin(); i != a.end();
+           ++i, ++j) {
+        array.init(j, *i);
+        init[j] = true;
+      }
+    }
+
+    if (inspector.cutoff()) {
+      assert(Shrink_Policy::value);
+      assert(array.end()-array.begin() == used_);
+      std::sort(array.begin(), array.end(), typename Inspector::compare());
+      if (array.begin() != array.end()) {
+        typename Vector_Sparse<T, U>::iterator last = array.begin();
+        typename Vector_Sparse<T, U>::iterator i = array.begin();
+        ++i;
+        U uniques = 1;
+        U newend = 1;
+        U k = inspector.k();
+        if (uniques < k) {
+          for (; i != array.end(); ++last, ++i) {
+            if (!inspector.equal_score(*last, *i))
+              ++uniques;
+            if (uniques > k)
+              break;
+            ++newend;
+          }
+        }
+        used_ = newend;
         Vector_Sparse<T, U> a(used_);
         swap(array, a);
         std::vector<bool> b(used_);
         swap(init, b);
         U j = 0;
-        for (typename Vector_Sparse<T, U>::iterator i = a.begin();
-            i != a.end(); ++i, ++j) {
-          array.init(j, *i);
+        for (typename Vector_Sparse<T, U>::iterator  x = a.begin();
+             j < newend; ++x, ++j) {
+          array.init(j, *x);
           init[j] = true;
         }
       }
-
-      if (inspector.cutoff()) {
-        assert(Shrink_Policy::value);
-        assert(array.end()-array.begin() == used_);
-        std::sort(array.begin(), array.end(), typename Inspector::compare());
-        if (array.begin() != array.end()) {
-          typename Vector_Sparse<T, U>::iterator last = array.begin();
-          typename Vector_Sparse<T, U>::iterator i = array.begin();
-          ++i;
-          U uniques = 1;
-          U newend = 1;
-          U k = inspector.k();
-          if (uniques < k) {
-            for (; i != array.end(); ++last, ++i) {
-              if (!inspector.equal_score(*last, *i))
-                ++uniques;
-              if (uniques > k)
-                break;
-              ++newend;
-            }
-          }
-          used_ = newend;
-          Vector_Sparse<T, U> a(used_);
-          swap(array, a);
-          std::vector<bool> b(used_);
-          swap(init, b);
-          U j = 0;
-          for (typename Vector_Sparse<T, U>::iterator  x = a.begin();
-               j < newend; ++x, ++j) {
-            array.init(j, *x);
-            init[j] = true;
-          }
-        }
-      }
     }
+  }
 
-    void finalize() {
+  void finalize() {
 #ifndef NDEBUG
-      assert(!finalized);
-      finalized = true;
+    assert(!finalized);
+    finalized = true;
 #endif
 
-      if (isEmpty())
-        return;
-
-      for (typename Vector_Sparse<T, U>::iterator i = array.begin();
-          i != array.end(); ++i)
-        inspector.finalize(*i);
+    if (isEmpty()) {
+      return;
     }
 
-    void *operator new(size_t t) noexcept(false);
-    void operator delete(void *b) noexcept(false);
+    for (typename Vector_Sparse<T, U>::iterator i = array.begin();
+         i != array.end(); ++i) {
+      inspector.finalize(*i);
+    }
+  }
+
+  void *operator new(size_t t) noexcept(false);
+  void operator delete(void *b) noexcept(false);
 };
 
 #define SET_TEMPLATE_DECL \
@@ -347,14 +374,12 @@ unsigned int load_factor
 #define SET_TEMPLATE_ARGS \
 T, I, U, Hash_Policy, Resize_Policy, Shrink_Policy, Stat_Policy, load_factor
 
-template<SET_TEMPLATE_DECL>
-  struct Set_Dummy {
-    static Pool<Set<SET_TEMPLATE_ARGS> > pool;
-  };
+template<SET_TEMPLATE_DECL> struct Set_Dummy {
+  static Pool<Set<SET_TEMPLATE_ARGS> > pool;
+};
 
-template<SET_TEMPLATE_DECL>
-   Pool<Set<SET_TEMPLATE_ARGS> >
-     Set_Dummy<SET_TEMPLATE_ARGS>::pool;
+template<SET_TEMPLATE_DECL> Pool<Set<SET_TEMPLATE_ARGS> >
+Set_Dummy<SET_TEMPLATE_ARGS>::pool;
 
 template<SET_TEMPLATE_DECL>
 void *Set<SET_TEMPLATE_ARGS>::operator new(size_t t) noexcept(false) {
@@ -365,14 +390,14 @@ void *Set<SET_TEMPLATE_ARGS>::operator new(size_t t) noexcept(false) {
 
 template<SET_TEMPLATE_DECL>
 void Set<SET_TEMPLATE_ARGS>::operator delete(void *b) noexcept(false) {
-  if (!b)
+  if (!b) {
     return;
+  }
   Set_Dummy<SET_TEMPLATE_ARGS>::pool.free(
-      static_cast<Set<SET_TEMPLATE_ARGS>*>(b));
+    static_cast<Set<SET_TEMPLATE_ARGS>*>(b));
 }
 
-template<class T, class I>
-class Ref : public ::Ref::Lazy<Set<T, I> > {
+template<class T, class I> class Ref : public ::Ref::Lazy<Set<T, I> > {
  private:
  public:
 };
@@ -389,66 +414,63 @@ inline void hash_filter(Hash::Ref<T, I> &x) {
   x->filter();
 }
 
-template<class T>
-inline void finalize(T &x) {
+template<class T> inline void finalize(T &x) {
 }
 
-template<class T, class I>
-inline void finalize(Hash::Ref<T, I> &x) {
+template<class T, class I> inline void finalize(Hash::Ref<T, I> &x) {
   x->finalize();
 }
 
-template<class T, class I>
-inline void push_back(Hash::Ref<T, I> &x, const T &e) {
+template<class T, class I> inline
+void push_back(Hash::Ref<T, I> &x, const T &e) {
   assert(is_not_empty(e));
   x->add(e);
 }
 
-template<class T, class I>
-inline void append(Hash::Ref<T, I> &x, Hash::Ref<T, I> &e) {
-  if (isEmpty(e))
+template<class T, class I> inline
+void append(Hash::Ref<T, I> &x, Hash::Ref<T, I> &e) {
+  if (isEmpty(e)) {
     return;
+  }
   assert(&x.ref() != &e.ref());
-  for (typename Hash::Ref<T, I>::iterator i = e->begin(); i != e->end(); ++i)
+  for (typename Hash::Ref<T, I>::iterator i = e->begin(); i != e->end(); ++i) {
     x->add(*i);
+  }
 }
 
-template<class T, class I, typename Iterator>
-inline void append_filter(Hash::Ref<T, I> &x, std::pair<Iterator, Iterator> i) {
-  for (Iterator a = i.first; a != i.second; ++a)
+template<class T, class I, typename Iterator> inline
+void append_filter(Hash::Ref<T, I> &x, std::pair<Iterator, Iterator> i) {
+  for (Iterator a = i.first; a != i.second; ++a) {
     push_back(x, *a);
+  }
   hash_filter(x);
 }
 
-template<class T, class I>
-inline void empty(Hash::Ref<T, I> &x) {
+template<class T, class I> inline void empty(Hash::Ref<T, I> &x) {
 }
 
-template<class T, class I>
-inline bool isEmpty(const Hash::Ref<T, I> &x) {
+template<class T, class I> inline bool isEmpty(const Hash::Ref<T, I> &x) {
   return !x.l || x.const_ref().isEmpty();
 }
 
-template<class T, class I>
-inline void erase(Hash::Ref<T, I> &x) {
+template<class T, class I> inline void erase(Hash::Ref<T, I> &x) {
 }
 
-template<class T, class U>
-inline void update_filter(T &x, const U &a) {
+template<class T, class U> inline void update_filter(T &x, const U &a) {
   x.update(a);
 }
 
-template<class T, class I>
-inline
+template<class T, class I> inline
 std::ostream &operator<<(std::ostream &out, Hash::Ref<T, I> &x) {
-  if (isEmpty(x))
+  if (isEmpty(x)) {
     return out;
+  }
   typename Hash::Ref<T, I>::Type &h = x.ref();
-  for (typename Hash::Ref<T, I>::iterator i = h.begin(); i != h.end(); ++i)
+  for (typename Hash::Ref<T, I>::iterator i = h.begin(); i != h.end(); ++i) {
     out << *i << '\n';
+  }
   return out;
 }
-
 
 #undef HASH_INITIAL
 #undef HASH_LOAD_FACTOR
