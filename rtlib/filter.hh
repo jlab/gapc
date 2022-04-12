363e8b3b57d626161319003924ba67ba4d8afbd5
Stefan Janssen <stefan.m.janssen@gmail.com>
2022-04-12 14:43:17 +0200

    cpplint

--- rtlib/filter.hh
+++ rtlib/filter.hh
@@ -128,8 +128,9 @@ inline bool samesize(const Basic_Sequence<a1, pos_type> &s1,
 }
 
 template<typename alphabet, typename pos_type, typename T>
-inline bool complete_track(const Basic_Sequence<alphabet, pos_type> &seq, T i, T j) {
-  return ((i == seq.n) and (j == seq.n));
+inline bool complete_track(
+    const Basic_Sequence<alphabet, pos_type> &seq, T i, T j) {
+  return ((i == seq.n) && (j == seq.n));
 }
 
 #endif  // RTLIB_FILTER_HH_
