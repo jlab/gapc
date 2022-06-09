9df703623ec68d432cc72029879e93be0aec88c2
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-06-09 10:29:39 +0200

    fix: cluster name

--- src/grammar.cc
+++ src/grammar.cc
@@ -1005,19 +1005,21 @@ void Grammar::remove(Symbol::NT *x) {
 unsigned int Grammar::to_dot(unsigned int *nodeID, std::ostream &out,
         int plot_grammar) {
   int start_node;
+  unsigned int i = 1;
   out << "digraph " << *this->name << " {\n";
   out << "compound = True;\n";
   out << "newrank=True;\n";
   out << "ordering=out;\n";
   for (std::list<Symbol::NT*>::const_iterator nt = this->nt_list.begin();
        nt != this->nt_list.end(); ++nt) {
-    out << "subgraph cluster_" << *this->name << "{\n";
+    out << "subgraph cluster_" << i << "{\n";
     start_node = (*nt)->to_dot(nodeID, out, false, this->axiom, plot_grammar);
     out << "}\n";
     if (start_node > 1) {
         out << "node_" << start_node-1 << " -> node_" << start_node <<
                 " [ style = invis];\n";
       }
+    i++;
   }
   out << "}\n";
   return ((unsigned int)*nodeID);
