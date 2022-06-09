a1ac6150a67cb08b46178f10c7aa8341834e34dc
Jasmin Walter <63170803+Jasmin-Walter@users.noreply.github.com>
2022-06-09 11:00:53 +0200

    Update multi_plot_grammar.cc

--- testdata/modtest/multi_plot_grammar.cc
+++ testdata/modtest/multi_plot_grammar.cc
@@ -73,6 +73,6 @@ int main(int argc, char **argv) {
   grammar->dep_analysis();
 
   unsigned int nodeID = 1;
-  int plot_level = 1
+  int plot_level = 1;
   grammar->to_dot(&nodeID, std::cout, plot_level);
 }
