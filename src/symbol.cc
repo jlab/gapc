49b4af6a4cb71a9cdfef1f2a17338023271c4d0c
Stefan Janssen <stefan.m.janssen@gmail.com>
2023-02-13 17:22:33 +0100

    restore special treatment of outside->inside transition + propagate outside grammar generation information

--- src/symbol.cc
+++ src/symbol.cc
@@ -753,7 +753,15 @@ void Symbol::NT::init_indices(Expr::Vacc *left, Expr::Vacc *right,
 
   for (std::list<Alt::Base*>::iterator i = alts.begin(); i != alts.end(); ++i) {
     if (this->is_partof_outside) {
-      (*i)->init_indices_outside(left, right, k, track, NULL, NULL, false);
+      if ((*i)->get_is_partof_outside() == false) {
+        // special treatment for a link to the original inside axiom, ensure
+        // that it has to consume the full input sequence(s)
+        (*i)->init_indices_outside(
+          left->plus(right), right->minus(left), k, track, NULL, NULL, false);
+        dynamic_cast<Alt::Link*>(*i)->init_outside_guards();
+      } else {
+        (*i)->init_indices_outside(left, right, k, track, NULL, NULL, false);
+      }
     } else {
       (*i)->init_indices(left, right, k, track);
     }
@@ -1228,7 +1236,7 @@ void Symbol::NT::codegen(AST &ast) {
   } else {
     f = new Fn_Def(dt, new std::string("nt_" + *name));
   }
-  f->add_para(*this);
+  f->add_para(*this, ast.grammar()->is_outside());
 
   subopt_header(ast, score_code, f, stmts);
 
