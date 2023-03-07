2dafa7848d497b6b6cb0d5211508fcd19a4d4c95
Stefan Janssen <stefan.m.janssen@gmail.com>
2023-03-07 18:52:38 +0100

    linting

--- src/outside/grammar_transformation.cc
+++ src/outside/grammar_transformation.cc
@@ -1,24 +1,47 @@
-/*
- * grammar_transformation.cc
- *
- *  Created on: Mar 7, 2023
- *      Author: sjanssen
- */
+/* {{{
 
+    This file is part of gapc (GAPC - Grammars, Algebras, Products - Compiler;
+      a system to compile algebraic dynamic programming programs)
+
+    Copyright (C) 2011-2023  Stefan Janssen
+         email: stefan.m.janssen@gmail.com or stefan.janssen@computational.bio.uni-giessen.de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+}}} */
+
+#include <vector>
+
+#include "grammar_transformation.hh"
 #include "../grammar.hh"
 #include "../ast.hh"
 
 bool Grammar::check_outside_parse_empty_word() {
   if (this->ast.outside_generation()) {
-    for (std::vector<Yield::Size>::const_iterator i = this->axiom->multi_ys().begin(); i != this->axiom->multi_ys().end(); ++i) {
+    for (std::vector<Yield::Size>::const_iterator i =
+         this->axiom->multi_ys().begin();
+         i != this->axiom->multi_ys().end(); ++i) {
       if ((*i).low() > 0) {
-    	std::ostringstream msg;
-        msg << "The minimal yield size of your grammar '" << *this->name << "' is ";
+        std::ostringstream msg;
+        msg << "The minimal yield size of your grammar '" << *this->name
+            << "' is ";
         (*i).low().put(msg);
         msg << ", i.e. it cannot parse the empty input string ''."
-            <<  " For outside grammar generation, this means you are lacking a "
-            << "recursion basis which will result in empty results for "
-            << "ALL outside candidates! Try adding an alternative like nil(EMPTY) to your axiom.";
+            << " For outside grammar generation, this means you are lacking a"
+            << " recursion basis which will result in empty results for "
+            << "ALL outside candidates! Try adding an alternative like "
+            << "nil(EMPTY) to your axiom.";
         Log::instance()->warning(this->location, msg.str());
         return false;
       }
