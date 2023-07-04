5cfa64477f078ba37e66f0ac742566875ae8be22
Stefan Janssen <stefan.m.janssen@gmail.com>
2023-07-04 15:53:20 +0200

    remove unused function

--- testdata/gapc_filter/isntimes.hh
+++ testdata/gapc_filter/isntimes.hh
@@ -2,13 +2,13 @@
 #define ISNTIMES_HH
 
 template<typename alphabet, typename pos_type, typename T>
-inline bool isntimes(const Basic_Sequence<alphabet, pos_type> &seq, T i, T j, alphabet x, unsigned int size)
-{
+inline bool isntimes(const Basic_Sequence<alphabet, pos_type> &seq, T i, T j,
+                     alphabet x, unsigned int size) {
   if (j<i)
     return false;
 
   if (j-i != size)
-	return false;
+    return false;
   
   for (T k = i; k < j; k++) {
     if (seq[k] != x)
@@ -17,14 +17,5 @@ inline bool isntimes(const Basic_Sequence<alphabet, pos_type> &seq, T i, T j, al
   
   return true;
 }
-
-//a filter to force that a parsed character corresponds to a nucleotide (and not a gap or some structure parts)
-template<typename T>
-inline bool isAnyBase(const Basic_Sequence<> &seq, T i, T j) {
-  if (j<i) return false;
-
-  return ((seq[i] == 'A')	|| (seq[i] == 'C') || (seq[i] == 'G') || (seq[i] == 'U') || (seq[i] == 'R') || (seq[i] == 'Y') || (seq[i] == 'M') || (seq[i] == 'K') || (seq[i] == 'W') || (seq[i] == 'B') || (seq[i] == 'D') || (seq[i] == 'H') || (seq[i] == 'V') || (seq[i] == 'N') || (seq[i] == 'S'));
-}
-
 
 #endif
