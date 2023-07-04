a2c366e09fc46bbb42850624d28d6b88da3f3f04
Stefan Janssen <stefan.m.janssen@gmail.com>
2023-07-04 15:54:07 +0200

    linting

--- testdata/gapc_filter/isntimes.hh
+++ testdata/gapc_filter/isntimes.hh
@@ -4,17 +4,17 @@
 template<typename alphabet, typename pos_type, typename T>
 inline bool isntimes(const Basic_Sequence<alphabet, pos_type> &seq, T i, T j,
                      alphabet x, unsigned int size) {
-  if (j<i)
+  if (j < i)
     return false;
 
   if (j-i != size)
     return false;
-  
+
   for (T k = i; k < j; k++) {
     if (seq[k] != x)
       return false;
   }
-  
+
   return true;
 }
 
