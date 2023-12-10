cb0e2e8b85a03c83e7c0a9a70decfe3ac1dcc340
Noah Stuertz <noah@coru.sh>
2023-12-10 11:11:30 +0100

    new flake

--- flake.nix
+++ flake.nix
@@ -1,52 +1,46 @@
 {
-  description = "Bellmans Gapc in Nix";
+  description = "Flake utils demo";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    # Systems supported
-    allSystems = [
-      "x86_64-linux" # 64-bit Intel/AMD Linux
-      "aarch64-linux" # 64-bit ARM Linux
-    ];
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          default = let
+            version = "2023.07.05";
+          in
+            pkgs.stdenv.mkDerivation {
+              name = "gapc-${version}";
 
-    # Helper to provide system-specific attributes
-    forAllSystems = f:
-      nixpkgs.lib.genAttrs allSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    packages = forAllSystems ({pkgs}: {
-      default = let
-        version = "2023.07.05";
-      in
-        pkgs.stdenv.mkDerivation {
-          name = "gapc-${version}";
+              src = pkgs.fetchFromGitHub {
+                owner = "jlab";
+                repo = "gapc";
+                rev = "${version}";
+                sha256 = "sha256-OzGf8Z4BzHEPmonlOvvvg5G1y1mtrkWSxOHPfBJU7kU=";
+              };
 
-          src = pkgs.fetchFromGitHub {
-            owner = "jlab";
-            repo = "gapc";
-            rev = "${version}";
-            sha256 = "sha256-OzGf8Z4BzHEPmonlOvvvg5G1y1mtrkWSxOHPfBJU7kU=";
-          };
+              buildInputs = with pkgs; [
+                gnumake
+                flex
+                bison
+                gsl
+                boost
+                mercurial
+              ];
 
-          buildInputs = with pkgs; [
-            gnumake
-            flex
-            bison
-            gsl
-            boost
-            mercurial
-          ];
-
-          configureFlags = ["--with-boost-libdir=${pkgs.boost.out}/lib"];
+              configureFlags = ["--with-boost-libdir=${pkgs.boost.out}/lib"];
+            };
         };
-    });
-  };
+      }
+    );
 }
