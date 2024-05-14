33114af179d8dded680e29224ed5bd060dd1e0a4
Noah Stuertz <noah@coru.sh>
2024-05-14 16:11:19 +0200

    added shell

--- shell.nix
+++ shell.nix
@@ -1,24 +1,25 @@
 {pkgs ? import <nixpkgs> {}}:
 with pkgs;
   mkShell {
-    nativeBuildInputs = [flex bison gsl];
     buildInputs = [
       nixpkgs-fmt
-      (callPackage ./package.nix {})
+      # (callPackage ./package.nix {})
       gdb
       gcc
       gpp
       lldb
       clang
-      boost
+      boost.dev
+      boost.out
       flex
       bison
+      bisoncpp
       gsl
       mercurial
     ];
 
-    configureFlags = ["BOOST_LDFLAGS=-L${boost}/lib"];
+    BOOST_LDFLAGS = "-L${boost}/lib";
     shellHook = ''
-      ./configure --prefix=$(pwd)/debug --with-boost=yes
+      # ./configure --prefix=$(pwd)/debug --with-boost=yes
     '';
   }
