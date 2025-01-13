eb65ee17eda9091dbb76e2557ad208e29f56b193
Noah Stuertz <noah@coru.sh>
2025-01-13 10:19:57 +0100

    added debug tooling

--- shell.nix
+++ shell.nix
@@ -3,12 +3,17 @@ with pkgs;
   mkShell {
     buildInputs = [
       nixpkgs-fmt
+      time
+      cmake
+
+      llvmPackages.openmp
+      llvmPackages.lldb
+      llvmPackages.clang
+
       # (callPackage ./package.nix {})
       gdb
       gcc
       gpp
-      lldb
-      clang
       boost.dev
       boost.out
       flex
@@ -16,10 +21,17 @@ with pkgs;
       bisoncpp
       gsl
       mercurial
+      valgrind
     ];
+
+    CXX = "clang++";
+    CC = "clang";
+    OMP_NUM_THREADS=8;
 
     BOOST_LDFLAGS = "-L${boost}/lib";
     shellHook = ''
+      export CXX=clang++;
+      export CC=clang
       # ./configure --prefix=$(pwd)/debug --with-boost=yes
     '';
   }
